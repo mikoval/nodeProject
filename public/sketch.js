d3b345c29fce0b16fc94ddbd9cb8424c97a30058
Michael Koval <mkoval3@gatech.edu>
2017-05-26 21:05:03 -0400

    pushing initial code for node project

---
+++ public/sketch.js
@@ -0,0 +1,54 @@
+var socket;
+
+var drawing = false;
+function setup() {
+  var myCanvas = createCanvas(600, 400);
+  myCanvas.parent('myContainer');
+  background(51);
+  socket = io.connect("http://localhost:3000")
+  socket.on('mouse', newDrawing);
+}
+
+
+function newDrawing(data){
+    noStroke();
+    fill(data.color.r, data.color.g, data.color.b, data.transparancy);
+    ellipse(data.x, data.y, data.radius, data.radius);
+
+}
+function mousePressed() {
+  drawing = true;
+}
+function mouseReleased(){
+    drawing = false;
+}
+function draw() {
+    if(drawing){
+        var color = "#" + document.getElementById("color-picker").value 
+        color = hexToRgb(color);
+        transparancy = parseInt(document.getElementById("transparency-picker").value );
+        fill(color.r, color.g, color.b, transparancy);
+        noStroke();
+        var radius = parseInt(document.getElementById("radius-picker").value);
+
+        ellipse(mouseX, mouseY, radius, radius)
+        var data = {
+            x:mouseX,
+            y:mouseY, 
+            radius:radius,
+            color:color,
+            transparancy:transparancy
+
+        }
+        socket.emit('mouse', data);
+    }
+    
+}
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
